68cb76507f987d8d02c93675c8f010fdaf66ec89
Doug Goldstein <cardoe@cardoe.com>
2015-09-17 09:27:19 -0500

    build: convert HAS_DEVICE_TREE use to Kconfig
    Use the Kconfig generated CONFIG_HAS_DEVICE_TREE defines in the code
    base.
    
    CC: Ian Campbell <ian.campbell@citrix.com>
    CC: Stefano Stabellini <stefano.stabellini@citrix.com>
    CC: Jan Beulich <jbeulich@suse.com>
    CC: Daniel De Graaf <dgdegra@tycho.nsa.gov>
    Signed-off-by: Doug Goldstein <cardoe@cardoe.com>
    Acked-by: Daniel De Graaf <dgdegra@tycho.nsa.gov>

--- xen/include/xsm/dummy.h
+++ xen/include/xsm/dummy.h
@@ -350,7 +350,7 @@ static XSM_INLINE int xsm_deassign_device(XSM_DEFAULT_ARG struct domain *d, uint
 
 #endif /* CONFIG_HAS_PASSTHROUGH && HAS_PCI */
 
-#if defined(CONFIG_HAS_PASSTHROUGH) && defined(HAS_DEVICE_TREE)
+#if defined(CONFIG_HAS_PASSTHROUGH) && defined(CONFIG_HAS_DEVICE_TREE)
 static XSM_INLINE int xsm_test_assign_dtdevice(XSM_DEFAULT_ARG const char *dtpath)
 {
     XSM_ASSERT_ACTION(XSM_HOOK);
@@ -371,7 +371,7 @@ static XSM_INLINE int xsm_deassign_dtdevice(XSM_DEFAULT_ARG struct domain *d,
     return xsm_default_action(action, current->domain, d);
 }
 
-#endif /* CONFIG_HAS_PASSTHROUGH && HAS_DEVICE_TREE */
+#endif /* CONFIG_HAS_PASSTHROUGH && CONFIG_HAS_DEVICE_TREE */
 
 static XSM_INLINE int xsm_resource_plug_core(XSM_DEFAULT_VOID)
 {
