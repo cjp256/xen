fa7e8f10b8bdad66c7fb1785b1b003507a92a4ad
Doug Goldstein <cardoe@cardoe.com>
2015-09-17 15:03:53 -0500

    build: convert HAS_MEM_SHARING use to Kconfig
    Use the Kconfig generated CONFIG_HAS_MEM_SHARING defines in the code base.
    
    Signed-off-by: Doug Goldstein <cardoe@cardoe.com>
    
    CC: Keir Fraser <keir@xen.org>
    CC: Jan Beulich <jbeulich@suse.com>
    CC: Andrew Cooper <andrew.cooper3@citrix.com>
    CC: Razvan Cojocaru <rcojocaru@bitdefender.com>
    CC: Tamas K Lengyel <tamas@tklengyel.com>
    CC: Daniel De Graaf <dgdegra@tycho.nsa.gov>

--- xen/common/memory.c
+++ xen/common/memory.c
@@ -1291,7 +1291,7 @@ int prepare_ring_for_helper(
         return -ENOENT;
     }
 #endif
-#ifdef HAS_MEM_SHARING
+#ifdef CONFIG_HAS_MEM_SHARING
     if ( p2m_is_shared(p2mt) )
     {
         if ( page )
